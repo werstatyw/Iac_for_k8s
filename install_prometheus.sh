91f41fc24f27ff2097ac4b694146668f601532b1
Alex G <a.grigoriev@skipp.pro>
2022-03-08 12:08:52 +0000

    grafana settings

--- install_prometheus.sh
+++ install_prometheus.sh
@@ -2,3 +2,5 @@ echo "adding repostory into helm:"
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 echo "updating added repository"
 helm repo update
+echo "installaton of prometheus"
+helm install prometheus prometheus-community/prometheus --namespace monitoring
