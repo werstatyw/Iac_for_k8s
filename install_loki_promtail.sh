47b0da3fb63133ea248a28db2051cd0c832a75d1
alex g <alexg@getcodified.com>
2022-03-22 12:03:59 +0100

    changes in promtail config in order to work.

--- install_loki_promtail.sh
+++ install_loki_promtail.sh
@@ -1,4 +1,4 @@
 echo "installing loki"
 helm upgrade --install loki grafana/loki --namespace=monitoring
 echo "installing promtail"
-helm upgrade --install promtail grafana/promtail --set "loki.serviceName=loki" -n monitoring
+helm upgrade --install promtail grafana/promtail --set config.lokiAddress=http://loki:3100/loki/api/v1/push  -n monitoring
