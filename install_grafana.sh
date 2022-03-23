45e4cb85b621a598bdc688a30a9241d294bc9d83
alex g <alexg@getcodified.com>
2022-03-23 08:25:11 +0100

    add persistence

--- install_grafana.sh
+++ install_grafana.sh
@@ -5,4 +5,4 @@ helm repo update
 echo "creating namespace for monitoring"
 kubectl create namespace monitoring
 echo "installing grafana. We are installing specific version for adding users (latest not working)"
-helm upgrade --install loki-grafana --namespace monitoring grafana/grafana --version 6.22.0 --set service.type=LoadBalancer
+helm upgrade --install loki-grafana --namespace monitoring grafana/grafana -f grafana_values.yaml --version 6.22.0 --set service.type=LoadBalancer
