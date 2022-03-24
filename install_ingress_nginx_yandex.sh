3b41748b11a30306d3af8d6cd0bdffbef3876831
alex g <alexg@getcodified.com>
2022-03-24 18:59:05 +0100

    editons for nginx installation

--- install_ingress_nginx_yandex.sh
+++ install_ingress_nginx_yandex.sh
@@ -3,4 +3,4 @@ helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 echo "updating helm..."
 helm repo update
 echo "installing ingress-nginx..."
-helm install ingress-nginx ingress-nginx/ingress-nginx
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx -f nginx_prometheus_enable.yaml
