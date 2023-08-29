9417353d6a84b22cd169924d6dd22226ce9a6b75
Alex G <a.grigoriev@ze.studio>
2023-08-29 06:13:20 +0200

    empty commit for a start

--- install_grafana.sh
+++
@@ -1,8 +0,0 @@
-echo "adding repository for helm charts"
-helm repo add grafana https://grafana.github.io/helm-charts
-echo "updating helm"
-helm repo update
-echo "creating namespace for monitoring"
-kubectl create namespace monitoring
-echo "installing grafana. We are installing specific version for adding users (latest not working)"
-helm upgrade --install loki-grafana --namespace monitoring grafana/grafana -f grafana_values.yaml --version 6.22.0 --set service.type=LoadBalancer
